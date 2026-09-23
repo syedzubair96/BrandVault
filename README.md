362e6a7f508fa47a90ad97fa8b33727f8691bc03
syedzubair96 <syedzubair95590@gmail.com>
2026-09-23 12:02:17 +0300

    project setup

--- README.md
+++
@@ -1,1 +0,0 @@
-# BrandVault-FE
---
+++ brand-vault/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex min-h-screen items-center justify-center bg-slate-100">
+        <h1 className="text-2xl font-bold text-blue-600 underline">
+          Hello World
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
